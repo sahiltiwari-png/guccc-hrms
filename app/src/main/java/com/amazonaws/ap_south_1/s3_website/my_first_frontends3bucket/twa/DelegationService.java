ef568bc6d95b5063894a2a9660beb9b431ba5c1e
sahiltiwari <sahiltiwaricontact9@gmail.com>
2025-10-28 18:29:00 +0530

    donedone

---
+++ app/src/main/java/com/amazonaws/ap_south_1/s3_website/my_first_frontends3bucket/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package com.amazonaws.ap_south_1.s3_website.my_first_frontends3bucket.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
